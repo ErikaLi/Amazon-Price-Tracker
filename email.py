9a8695bb040d8b088c1fed562d954fbcae42e083
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-12 17:39:08 -0800

    added resources to set up email job in python

---
+++ email.py
@@ -0,0 +1,5 @@
+# http://www.pythonforbeginners.com/code-snippets-source-code/using-python-to-send-email
+# https://motherboard.vice.com/en_us/article/pgkbn9/how-to-send-an-email-from-a-python-script/
+import smtplib
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
